6afdae417e2efa0002032b7b97608d0ff19553ef
decster1488 <decster1488@gmail.com>
2018-07-25 17:09:18 +0300

    swals configuration

--- src/app/views/front-page/signup/signup.component.ts
+++ src/app/views/front-page/signup/signup.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {UserServiceService} from '../../../shared/services/user-service.service';
+import {SwalComponent} from '@toverux/ngx-sweetalert2';
 
 @Component({
   selector: 'app-signup',
@@ -11,16 +12,50 @@ export class SignupComponent implements OnInit {
   public userName = '';
   public userMail = '';
   public password = '';
+  public passwordRepit = '';
+  swalHtml: any;
+
+  @ViewChild('signusSwal') private createSwal: SwalComponent;
+
 
   constructor(private userserv: UserServiceService) { }
 
   ngOnInit() {
   }
 
-  okayPress() {
-  const req = { username: this.userName, email: this.userMail, password: this.password };
-  console.log(req);
-  this.userserv.registerUser(req).subscribe(res => {});
+
+  openHtml() {
+    this.setSwalHtml()
+    this.createSwal.html = this.swalHtml;
+    this.createSwal.show();
   }
+
+  userSignup() {
+  // const req = { username: this.userName, email: this.userMail, password: this.password };
+  // console.log(req);
+  // this.userserv.registerUser(req).subscribe(res => {});
+    console.log('KY');
+  }
+
+  setSwalHtml() {
+
+    this.swalHtml =
+      `
+        <div>
+          <p>Enter your Name:</p>
+          <input type="text" placeholder="Name" [(ngModel)]="userName" autofocus>
+          <p>Entre your E-mail:</p>
+          <input type="text" placeholder="E-mail" [(ngModel)]="userMail">
+          <p>Enter password:</p>
+          <input type="password" placeholder="Password" [(ngModel)]="password">
+          <p>Repeat password:</p>
+          <input type="password" placeholder="Repeat password" [(ngModel)]="passwordRepit">
+          <p>If everything ok press Sign up</p>
+        </div>
+      `
+  }
+
+
+
 
 }
